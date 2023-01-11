0c93ed0070c8e3c5c14ddf2e50e72579e6260038
Yesua <65790207+YesuaPV@users.noreply.github.com>
2023-01-11 09:27:44 -0600

    Practica4

--- Practica4/FTP.py
+++ Practica4/FTP.py
@@ -10,40 +10,52 @@ def iniciaFTP():
     ftp.connect(HOST)
     ftp.login(input("Usuario: "), input("Password: "))
     print(ftp.getwelcome())
-    op = 0
-    while op == 0:
-        cmd = input("cmd: ")
-        comandos = cmd.split(" ")
-        try:
-            match comandos[0]:
-                case "cd":
-                    print(ftp.cwd(comandos[1]))
-                case "rm":
-                    if comandos[1].contains('.'):
-                        ftp.delete(comandos[1])
-                    else:
-                        ftp.rmd(comandos[1])
-                case "ls":
-                    files = []
-                    ftp.dir(files.append)
-                    for f in files:
-                        print(f)
-                case "pwd":
-                    print(ftp.pwd())
-                case "mkd":
-                    ftp.mkd(comandos[1])
-                case "mv":
-                    ftp.rename(comandos[1], comandos[2])
-                case "put":
-                    with open(dir+comandos[1], 'rb') as file:
-                        ftp.storbinary('STOR '+comandos[2], file)
-                case "get":
-                    with open(dir+comandos[2], 'wb') as file:
-                        ftp.retrbinary("RETR " + comandos[1], file.write)
-                case "exit":
-                    op = 1
-                case default:
-                    print("Comando invalido")
-        except all_errors as e:
-            print(f'\033[91m Error en FTP: {e} \033[0m')
+    op = 1
+    while (op < 3 and op > 0):
+        op = int(input("Selecciones su opción \n1)obtener startup.conf \n2)subir startup.conf \notro)salir\n"))
+        match op:
+            case 1:
+                with open(dir + "startup-config", 'wb') as file:
+                 ftp.retrbinary("RETR " + "startup-config", file.write)
+                op = 5
+            case 2:
+                with open(dir + "startup-config", 'rb') as file:
+                 ftp.storbinary('STOR '+"startup-config", file)
+                op = 5
+            case default:
+                return
+    #while op == 0:
+       # cmd = input("cmd: ")
+        #comandos = cmd.split(" ")
+       # try:
+         #   match comandos[0]:
+         #       case "cd":
+         #           print(ftp.cwd(comandos[1]))
+          #      case "rm":
+          #          if comandos[1].contains('.'):
+          #              ftp.delete(comandos[1])
+           #         else:
+          #              ftp.rmd(comandos[1])
+         #       case "ls":
+         #           files = []
+         #           ftp.dir(files.append)
+         #           for f in files:
+         #               print(f)
+         #       case "pwd":
+         #           print(ftp.pwd())
+         #       case "mkd":
+         #           ftp.mkd(comandos[1])
+         #       case "mv":
+         #           ftp.rename(comandos[1], comandos[2])
+         #       case "put":
+         #           with open(dir+comandos[1], 'rb') as file:
+        #                ftp.storbinary('STOR '+comandos[2], file)
+        #        case "get":
+        #            with open(dir+comandos[2], 'wb') as file:
+        #                ftp.retrbinary("RETR " + comandos[1], file.write)
+        #             op = 1
+       #         case default:
+       #             print("Comando invalido")
+      #  except all_errors as e:
+     #       print(f'\033[91m Error en FTP: {e} \033[0m')
     ftp.close()
--- Practica4/Main.py
+++ Practica4/Main.py
@@ -5,7 +5,7 @@ import pyautogui
 op = 1
 while(op < 3 and op > 0):
   print('\033[92m' + "\n\tAdministración de configuracion")
-  print("\tPráctica 4: Administración de Contabilidad")
+  print("\tPráctica 4: Administración de Configuracion")
   print("\tPérez Vidales Yesua David \t4CM13\t2019630501" + '\033[0m')
 
   print("\nMENU")
